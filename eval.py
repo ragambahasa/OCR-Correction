78c5dd0c19b4d2c288b0472bd279e5174d14e10e
Rifqi222 <mrifki193@gmail.com>
2024-08-05 14:42:02 +0700

    feat: new print method

--- eval.py
+++ eval.py
@@ -49,10 +49,9 @@ def compile_dataset(gt_dir, engine_dir, **kwargs):
         df.to_csv(filename, sep=";")
     return df
 
-def evaluation_metrics(df, **kwargs):
+def evaluation_metrics(title, df, **kwargs):
     df['max_len'] = df.apply(lambda x: max(tuple((len(x['text_engine']), len(x['text_gt'])))), axis=1)
     df['levd'] = df.apply(lambda x: jellyfish.levenshtein_distance(x['text_engine'], x['text_gt']), axis=1)
-    # df['levd_wer'] = df.apply(lambda x: 1-x['levd']/len(x['text_gt']), axis=1)
     df['levd_score'] = df.apply(lambda x: 1-x['levd']/x['max_len'], axis=1)
 
     df['wer'] = df.apply(lambda x: 1-wer(x['text_gt'], x['text_engine']), axis=1)
@@ -87,11 +86,15 @@ def evaluation_metrics(df, **kwargs):
     cer_mix = df.loc[df['cer'].idxmin()]
     mer_mix = df.loc[df['mer'].idxmin()]
     levd_score_mix = df.loc[df['levd_score'].idxmin()]
-
-    print(f"WER Average: {wer_avg:.4f}, Min: {wer_min:.4f}, Max: {wer_max:.4f}, Mix: {wer_mix['wer']:.4f}")
-    print(f"CER Average: {cer_avg:.4f}, Min: {cer_min:.4f}, Max: {cer_max:.4f}, Mix: {cer_mix['cer']:.4f}")
-    print(f"MER Average: {mer_avg:.4f}, Min: {mer_min:.4f}, Max: {mer_max:.4f}, Mix: {mer_mix['mer']:.4f}")
-    print(f"Levenshtein Score Average: {levd_score_avg:.4f}, Min: {levd_score_min:.4f}, Max: {levd_score_max:.4f}, Mix: {levd_score_mix['levd_score']:.4f}")
+    
+    print(" " + "-" * 46)
+    print("|" + " " * ((46 - len(title)) // 2) + title + " " * ((46 - len(title)) // 2) + "|")
+    print(" " + "-" * 46)
+    print("|" + " " * 5 + "|" + " WAR ".center(9) + "|" + " CAR ".center(9) + "|" + " MAR ".center(9) + "|" + " LEV ".center(9) + "|")
+    print("|" + " AVG ".center(5) + "|" + f"{wer_avg: 7.4f}".center(9) + "|" + f"{cer_avg: 7.4f}".center(9) + "|" + f"{mer_avg: 7.4f}".center(9) + "|" + f"{levd_score_avg: 7.4f}".center(9) + "|")
+    print("|" + " MAX ".center(5) + "|" + f"{wer_max: 7.4f}".center(9) + "|" + f"{cer_max: 7.4f}".center(9) + "|" + f"{mer_max: 7.4f}".center(9) + "|" + f"{levd_score_max: 7.4f}".center(9) + "|")
+    print("|" + " MIN ".center(5) + "|" + f"{wer_min: 7.4f}".center(9) + "|" + f"{cer_min: 7.4f}".center(9) + "|" + f"{mer_min: 7.4f}".center(9) + "|" + f"{levd_score_min: 7.4f}".center(9) + "|")
+    print(" " + "-" * 46)
 
     return df
 
@@ -107,12 +110,12 @@ def main():
     TEST_ANN_DIR = f"{args.ann}"
     TEST_CORRECTION_DIR = f"{args.post}"
 
-    print("POST OCR")
+    title = "Post Ocr Correction"
     compiled = compile_dataset(gt_dir=TEST_ANN_DIR, engine_dir=TEST_CORRECTION_DIR)
-    evaluate = evaluation_metrics(df=compiled, export="eval-result")
-    print("\nOTS")
+    evaluate = evaluation_metrics(title, df=compiled, export="eval-result")
+    title = "Off The Shelf"
     compiled = compile_dataset(gt_dir=TEST_ANN_DIR, engine_dir=TEST_OTS_DIR)
-    evaluate = evaluation_metrics(df=compiled, export="eval-result")
+    evaluate = evaluation_metrics(title, df=compiled, export="eval-result")
     
 if __name__ == "__main__":
     main()
